bd1a74b3e4cebb5b8e978cab396e8f02c5adeb91
Razieh Abedi <raziyeabedi@gmail.com>
2022-11-16 17:53:01 +0800

    debugged the router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AppProvider } from "./context";
-// import Movie from "./SingleMovie";
+import Movie from "./SingleMovie";
 import Home from "./Home";
 function App() {
   return (
     <BrowserRouter>
       <AppProvider>
         <Routes>
-          {/* <Route path="./" element={<Home />} />
-          <Route path="movies/:id" element={<Movie />} /> */}
-          <Route element={<Home/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="movies/:id" element={<Movie />} />
+          {/* <Route element={<Home/>}>
+
+          </Route> */}
         </Routes>
       </AppProvider>
     </BrowserRouter>
